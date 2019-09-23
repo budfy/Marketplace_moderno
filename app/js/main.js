7dce1b642bc0c29ae5b514447f42df6678e328f5
budfy <serdyuk.ro@gmail.com>
2019-09-24 00:00:19 +0300

    Added product-section and product slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,21 @@
 $(function(){
 
 
+  var mixer = mixitup('.products__inner-box');
 
+  $(".rate-star").rateYo({
+    rating: 5,
+    starWidth: "12px",
+    ratedFill: "#ffa726",
+    readOnly: true,
+    spacing: "3px",
+  });
 
+  $('.product-slider__inner').slick({
+    dots: true,
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 4
+  });
 
 });
