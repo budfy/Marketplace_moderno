d95d5bf7f43a81ec461c4aa2306588a8521ba2e8
budfy <serdyuk.ro@gmail.com>
2019-10-14 13:40:52 +0300

    Author box list bug

--- app/js/main.js
+++ app/js/main.js
@@ -76,4 +76,13 @@ $('.menu').toggleClass('active');
 
 });
 
+$('.product-one__tabs .tab').on('click', function(event) {
+  var id = $(this).attr('data-id');
+    $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+    $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+    $(this).addClass('active');
+    $('#'+id).addClass('active-tab').fadeIn();
+    return false;
+  });
+
 });
